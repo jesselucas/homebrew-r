5b40772f461788536ba5b80013168ab36d73ca3a
Jesse Lucas <jesse@forestgiant.com>
2016-01-10 17:24:47 -0500

    Updating to r 0.4.0

--- r.rb
+++ r.rb
@@ -1,9 +1,9 @@
 class R < Formula
   desc "r is a contextual, path based, bash history."
   homepage "https://jesselucas.github.io/r/"
-  url "https://github.com/jesselucas/r/archive/0.3.3.tar.gz"
-  version "0.3.3"
-  sha256 "d5eefc25908e56400d7af069d3b979a2f81547f04c2d38e15366f34e467711ff"
+  url "https://github.com/jesselucas/r/archive/0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "b82476b4c7b64ab789f4918ab64a6766ae4be06b5553792a2ed0c1342ad7e442"
 
   depends_on "go" => :build
 
@@ -11,8 +11,8 @@ class R < Formula
     ENV["GOPATH"] = buildpath
     ENV["GO15VENDOREXPERIMENT"] = "1"
 
-    (buildpath/"src/github.com/jesselucas/r/").install Dir["*"]
-    system "go", "build", "-o", "#{bin}/r", "-v", "github.com/jesselucas/r/"
+    (buildpath/"src/github.com/jesselucas/r/cmd/r").install Dir["*"]
+    system "go", "build", "-o", "#{bin}/r/cmd/r", "-v", "github.com/jesselucas/r/cmd/r"
   end
 
   def caveats; <<-EOS.undent
@@ -23,7 +23,7 @@ class R < Formula
   test do
     actual = system("r -install")
     expected = true
-    actualVersion = pipe_output("#{bin}/r -version")
+    actualVersion = pipe_output("#{bin}/r/cmr/r -version")
     assert_block do
       actual == expected
       actualVersion == version
